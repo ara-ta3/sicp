4f13a9145dc7b6501141f64bfe795a58e349543f
Arata Tanaka <tarata43@yahoo.co.jp>
2014-08-26 21:04:13 +0900

    眠いから終了

---
+++ 1st/ques6.scm
@@ -0,0 +1,33 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+  (else else-clause))
+)
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+  guess
+  (sqrt-iter (improve guess x) x))
+)
+
+(define (improve guess x)
+  (average guess (/ x guess))
+)
+
+(define (average x y)
+  (/ (+ x y) 2)
+)
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001)
+)
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x)
+)
+
+(define (square x) (* x x))
+
+(print (new-if (= 2 3) 0 5))
+(print (new-if (= 1 1) 0 5))
+
+(print (sqrt 9))
