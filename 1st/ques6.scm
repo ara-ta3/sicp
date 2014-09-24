23106ebb94c618cb74c6cc37637668e51ae792c1
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2014-09-24 20:21:48 +0900

    1.6

--- 1st/ques6.scm
+++ 1st/ques6.scm
@@ -4,28 +4,62 @@
 )
 
 (define (sqrt-iter guess x)
-  (new-if (good-enough? guess x)
-  guess
-  (sqrt-iter (improve guess x) x))
+  (new-if (< (abs (- (* guess guess) x)) 0.001)
+    guess
+  (sqrt-iter (/ (+ guess (/ x guess)) 2) x))
 )
+; new-ifを展開
+; sqrt-iterを展開
+; new-ifを展開、以後ループ？
 
-(define (improve guess x)
-  (average guess (/ x guess))
-)
+;(define (sqrt-iter guess x)
+;  (cond ( (< (abs (- (* guess guess) x)) 0.001) guess)
+;  (else sqrt-iter (/ (+ guess (/ x guess)) 2) x))
+;)
+;こっちは
+; condが見えたので、述語を評価、elseになったので、sqrt-iterを評価
+; でゴール
+; ということ？
 
-(define (average x y)
-  (/ (+ x y) 2)
-)
+; 特殊形式は独自の評価順序を持つ
+; ifもまたそう
+; ifはpredicateを評価し、その値を返す
+; condもそう
+; predicateを評価し続け、当てはまったものの帰結式を評価した値を返す
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001)
-)
 
 (define (sqrt x)
   (sqrt-iter 1.0 x)
 )
+;(define (sqrt-iter guess x)
+;  (new-if (good-enough? guess x)
+;    guess
+;  (sqrt-iter (improve guess x) x))
+;)
 
-(define (square x) (* x x))
+;(define (improve guess x)
+;  (/ (+ guess (/ x guess)) 2)
+;)
+;
+;(define (good-enough? guess x)
+;  (< (abs (- (* guess guess) x)) 0.001)
+;)
+
+;(define (improve guess x)
+;  (average guess (/ x guess))
+;)
+;
+;(define (average x y)
+;  (/ (+ x y) 2)
+;)
+
+
+;(define (good-enough? guess x)
+;  (< (abs (- (square guess) x)) 0.001)
+;)
+;
+;(define (square x) (* x x))
+
 
 (print (new-if (= 2 3) 0 5))
 (print (new-if (= 1 1) 0 5))
