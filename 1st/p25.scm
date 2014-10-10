bf3d276adcdf71499ab7c394f15843b050c4042e
Arata Tanaka <tarata43@yahoo.co.jp>
2014-10-10 21:27:23 +0900

    fast-expt

---
+++ 1st/p25.scm
@@ -0,0 +1,26 @@
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (square x) (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(use slib)
+(require `trace)
+(trace fast-expt)
+
+(print (fast-expt 2 10))
+
+
+(define (expt b n)
+  (if (= n 0)
+    1
+    (* b (expt b (- n 1)))))
+
+(trace expt)
+(print (expt 2 10))
+
+
