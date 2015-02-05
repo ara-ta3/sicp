5b647414ecd5e0211af903606066d612c0e5c918
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-02-05 21:45:47 +0900

    問38 39

---
+++ 1st/ques38.scm
@@ -0,0 +1,10 @@
+(load "./ques37.scm")
+
+;(print (+ (cont-frac (lambda (i) 1.0)
+;                  (lambda (i) (if (= (remainder i 3) 2)
+;                                 (* (/ (+ 1 i) 3) 2)
+;                                 1))
+;                  1000) 2))
+
+;2.7182818284590455
+
