431693992f9fef91524cdcadbd52b54e243b3874
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-02-12 21:28:43 +0900

    問41

---
+++ 1st/ques41.scm
@@ -0,0 +1,12 @@
+(load "./lib/commons")
+
+(define (double f)
+    (lambda (x) (f (f x))))
+
+(print ((double inc) 10))
+
+(print (((double (double double)) inc) 5))
+;(((double (double double)) inc) 5)
+;(((double doubledouble) inc) 5)
+;((doubledoubledoubledouble inc) 5)
+;2回作用させるのを4回 なので2^4
