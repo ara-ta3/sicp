e9ee488d8c2e3910c2f9097e9b00fa9ce7cdef80
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-02-19 21:08:51 +0900

    p49まで
    既約分数にはならない

---
+++ 2nd/p45.scm
@@ -0,0 +1,44 @@
+(define (linear-combination a b x y)
+  (+ (* a x) (* b y)))
+
+(define (linear-combination a b x y)
+  (add (mul a x) (mul b y)))
+
+(define (add +))
+(define (mul +))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+
+;(print-rat one-half)
+
+
---
+++ 2nd/test/p45.scm
@@ -0,0 +1,17 @@
+(use gauche.test)
+(load "./p45.scm")
+
+(test "add-rat" (cons 5 6) (lambda () (add-rat (make-rat 1 2) (make-rat 1 3))))
+
+(test "sub-rat" (cons 1 6) (lambda () (sub-rat (make-rat 1 2) (make-rat 1 3))))
+
+(test "mul-rat" (cons 3 2) (lambda () (mul-rat (make-rat 1 2) (make-rat 3 1))))
+
+(test "div-rat" (cons 3 2) (lambda () (div-rat (make-rat 1 2) (make-rat 1 3))))
+
+(test "make-rat" (cons 1 2) (lambda () (make-rat 1 2)))
+
+(test "denom" 2 (lambda () (denom (make-rat 1 2))))
+
+(test "numer" 1 (lambda () (numer (make-rat 1 2))))
+
