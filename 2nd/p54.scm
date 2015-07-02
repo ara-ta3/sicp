e89303dc78f8d567ad905da197bdc63c692740d3
Arata Tanaka <tarata43@yahoo.co.jp>
2015-07-02 19:47:33 +0900

    :goat:

--- 2nd/p54.scm
+++ 2nd/p54.scm
@@ -7,3 +7,15 @@
 (define (width i)
   (/ (- (upper-bound i) (lower-bound i)) 2))
 
+(load "./p53.scm")
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+                  (add-interval (div-interval one r1)
+                                (div-interval one r2)))))
+
