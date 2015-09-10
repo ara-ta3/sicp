791560975e17a96749bf7f5538d4a89993cb918f
Arata Tanaka <tarata43@yahoo.co.jp>
2015-09-10 21:06:32 +0900

    Q24 + p62

---
+++ 2nd/p61.scm
@@ -0,0 +1,8 @@
+(define x (cons (list 1 2) (list 3 4)))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
